289ac54e9e14b9942b4770ef54ed413bfdd09479
hilderbatis77-crypto <hilderbatis77@gmail.com>
2026-01-07 03:31:16 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,18 @@
 import os
+import time
 from telegram import Bot
 
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 CANAL_ID = os.getenv("CANAL_ID")
 
 bot = Bot(token=TOKEN)
-bot.send_message(chat_id=CANAL_ID, text="🚀 Bot online no Render!")
+
+# envia mensagem de teste
+bot.send_message(
+    chat_id=CANAL_ID,
+    text="✅ Bot conectado com sucesso! Render funcionando 🚀"
+)
+
+# mantém o bot vivo no Render
+while True:
+    time.sleep(60)
