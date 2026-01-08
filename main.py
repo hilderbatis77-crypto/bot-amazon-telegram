ddf8ae70bc740131e034c8a848df92d08199e48a
hilderbatis77-crypto <hilderbatis77@gmail.com>
2026-01-08 08:20:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,14 @@
-import os
-from telegram import Update
-from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
+from telegram import Bot
+import time
 
-TOKEN = os.getenv("BOT_TOKEN")
-CHANNEL_ID = os.getenv("CHANNEL_ID")
+BOT_TOKEN = "8525612178:AAHon74pKlOfLYfu3meUmOKhlmES3-trIIY"
+CHANNEL_ID = "@achadosdokick"
 
-async def encaminhar(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if not update.message or not update.message.text:
-        return
+bot = Bot(token=BOT_TOKEN)
 
-    texto = update.message.text
+bot.send_message(
+    chat_id=CHANNEL_ID,
+    text="🚀 TESTE OK!\n\nMensagem enviada pelo Railway com sucesso."
+)
 
-    if "amazon." in texto or "amzn.to" in texto:
-        mensagem = (
-            "🔥 ACHADO NA AMAZON 🔥\n\n"
-            "🛒 Produto recomendado\n"
-            f"👉 {texto}\n\n"
-            "⚡ Oferta por tempo limitado!"
-        )
-
-        await context.bot.send_message(
-            chat_id=CHANNEL_ID,
-            text=mensagem
-        )
-
-def main():
-    app = ApplicationBuilder().token(TOKEN).build()
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, encaminhar))
-    print("🤖 Bot iniciado com sucesso")
-    app.run_polling()
-
-if __name__ == "__main__":
-    main()
+time.sleep(10)
