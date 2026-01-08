db5775c143996e036968b0c07c5649ff9f9a0c8f
hilderbatis77-crypto <hilderbatis77@gmail.com>
2026-01-08 10:41:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -128,7 +128,8 @@ async def receber_link(update: Update, context: ContextTypes.DEFAULT_TYPE):
 def main():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, receber_link))
-    app.run_polling()
+    app.run_polling(close_loop=false)
+
 
 if __name__ == "__main__":
     main()
